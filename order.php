6665d18088b90fc94447428d7c84a80750a9ff1b
renceng <renceng@sina.com>
2018-12-10 15:53:05 +0800

    Update order.php

--- order.php
+++ order.php
@@ -14,7 +14,8 @@ class OrderController extends AppBaseController
         parent::_initialize();
     }
 
-    public function wx_pay(){
+    public function wx_pay()
+    {
         Vendor('Weixinpay.Wechatpay');
         $wx_pay = new \Wechatpay();
         $body = '测试支付';
@@ -24,11 +25,12 @@ class OrderController extends AppBaseController
         $res = $wx_pay->getPrePayOrder($body,$out_trade_no,$total_fee,$notify_url);
     }
   
-  public function neworderNum(){
-    $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
-    $orderSn = $yCode[intval(date('Y')) - 2011] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('%02d', rand(0, 999999));
-    return $orderSn;
-  }
+    public function neworderNum()
+    {
+        $yCode = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+        $orderSn = $yCode[intval(date('Y')) - 2011] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('%02d', rand(0, 999999));
+        return $orderSn;
+    }
 
 
 }
